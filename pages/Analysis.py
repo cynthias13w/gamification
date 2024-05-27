e984ed6ee7abfdecf21ac83b0954f32233f035bf
cynthias13w <mcynsiew@gmail.com>
2024-05-27 22:58:54 +0400

    Requirements and functions

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -1,7 +1,65 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-from functions import *
+def print_badge_associations(column_name):
+    """
+    Print the names of badges associated with the given column name.
+
+    Parameters:
+        column_name (str): The column name for which to print badge associations.
+    """
+    badge_associations = {
+        "EVENTS_CREATED": "📆 Event Planner",
+        "EVENT_PARTICIPANTS": "📆 Event Planner",
+        "POSTS_CREATED": "🗣️ Conversation Starter",
+        "REPLIES_RECEIVED": "🗣️ Conversation Starter",
+        "ITEMS_GIFTED": "🎁 Philanthropist",
+        "THANKYOUS_RECEIVED": "🫱🏻 Helping Hand",
+        "PLACES_RECOMMENDED": "🧳 Local Guide"
+    }
+
+    if column_name in badge_associations:
+        print(f"{badge_associations[column_name].upper()}")
+    else:
+        print(f"No badge association found for '{column_name}'.")
+
+def visualize_columns(df, column_names):
+    """
+    Visualize specified columns from the DataFrame using seaborn countplot.
+
+    Parameters:
+        df (DataFrame): The DataFrame containing the data.
+        column_names (list): A list of column names to visualize.
+    """
+
+    for column_name in column_names:
+        # Print associated badge name
+        # print_badge_associations(column_name)
+
+        plt.figure(figsize=(8, 6))
+        ax = sns.countplot(x=df[column_name])
+        #plt.title(f"Countplot of {column_name}")
+
+        # Set integer x-axis tick labels
+        plt.xticks(rotation=45)  # Rotate labels for better visibility
+        plt.gca().xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: int(x)))
+
+        # Add count labels to the bars
+        for p in plt.gca().patches:
+            plt.gca().annotate(f'{int(p.get_height())}', (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='bottom', fontsize=10, color='black', xytext=(0, 5), textcoords='offset points')
+
+        sns.despine(bottom=True, right=True, left=True)
+        #ax.set(yticklabels=[])
+
+        plt.tick_params(axis='both', which='both', bottom=False, left=False)
+        plt.xlim(1, 25)
+        plt.ylim(0, df[column_name].value_counts()[1.0])
+        # plt.ylim(0, 169)
+        plt.show()
+
+        ax.set_ylabel(None)
 
 
 # st.markdown('## Analysis')
